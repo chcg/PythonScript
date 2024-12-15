901c933ec951dcc45bf610802a45c6dbe2a6f9af
Christian Grasser <christian.grasser@live.de>
2024-12-15 20:48:55 +0100

    further test

--- installer/buildInstaller.bat
+++ installer/buildInstaller.bat
@@ -140,8 +140,10 @@ IF NOT EXIST "build\%PYTHONSCRIPTVERSION%" (
 	mkdir build\%PYTHONSCRIPTVERSION%
 )
 
+cd ..\packages
 wix extension add WixToolset.UI.wixext
 wix extension list
+cd ..\installer
 
 wix build %INST_TEMP_DIR%\pythonscript.wixlib %INST_TEMP_DIR%\fullLib.wixlib %INST_TEMP_DIR%\fullLib_dll%NAME_ADDON%.wixlib %INST_TEMP_DIR%\unittests.wixlib %INST_TEMP_DIR%\tcl.wixlib %INST_TEMP_DIR%\tcl_dll%NAME_ADDON%.wixlib %INST_TEMP_DIR%\sampleScripts.wixlib %INST_TEMP_DIR%\htmldoc.wixlib -o build\%PYTHONSCRIPTVERSION%\PythonScript_%PYTHONSCRIPTVERSION%%NAME_ADDON%.msi -ext WixToolset.UI.wixext
 if NOT [%ERRORLEVEL%]==[0] (
